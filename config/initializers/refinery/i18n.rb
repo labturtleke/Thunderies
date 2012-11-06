38ba3a88c9f0507eebc93fc2a66945ffa225ace8
labturtleke <puskas.eva@thunderies.com>
2012-11-06 23:29:06 +0100

    Created application using Refinery CMS 2.0.8

---
+++ config/initializers/refinery/i18n.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+Refinery::I18n.configure do |config|
+  # config.enabled = true
+
+  # config.default_locale = :en
+
+  # config.current_locale = :en
+
+  # config.default_frontend_locale = :en
+
+  # config.frontend_locales = [:en]
+
+  # config.locales = {:"zh-TW"=>"Traditional Chinese", :pl=>"Polski", :"pt-BR"=>"Português", :it=>"Italiano", :nl=>"Nederlands", :en=>"English", :de=>"Deutsch", :lv=>"Latviski", :fr=>"Français", :sk=>"Slovenský", :rs=>"Srpski", :nb=>"Norsk Bokmål", :da=>"Dansk", :cs=>"Česky", :ru=>"Русский", :ja=>"日本語", :sl=>"Slovenian", :el=>"Ελληνικά", :"zh-CN"=>"Simplified Chinese", :sv=>"Svenska", :bg=>"Български", :es=>"Español"}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thunderies::Application.config.secret_token = 'c3baa20a49b560c18fb49b954ddf778787090a98345b827b43a591d56c97257b48c46d4d34d38e3137e5cfa5dec10edfefc41b4559e3010cf07dca5cfca60a5d'
